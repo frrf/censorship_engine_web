8896c8d7f82a6f33142cb367f285e68c454b3517
frrf <franciscopali71@gmail.com>
2020-04-23 19:18:30 -0700

    User input/output
    added {{processed_text}} to display user output in textarea "output" object
    
    note: user input is erased due to a refreshing of the page when passing the output to textarea

--- app_runner.py
+++ app_runner.py
@@ -1,10 +1,19 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template, request
 
+# App config.
 app = Flask(__name__)
 
+##########################################################################
+
 @app.route("/")
-def home():
+def index():
   return render_template("index.html")
+
+@app.route("/", methods=["POST"])
+def user_input_post():
+  text = request.form["text"]
+  processed_text = text.upper()
+  return render_template("index.html", processed_text = processed_text)
 
 
 
