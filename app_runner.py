2ba740c0fb050163c4c370d1a2c9940ba77e8e9d
frrf <franciscopali71@gmail.com>
2020-04-23 21:30:52 -0700

    Functioning Censor
    Added Censorship function, however only one term is censored because the list profane_terms only contians one word.
    
    To do: use a databse of curse words to populate profane_terms

--- app_runner.py
+++ app_runner.py
@@ -5,14 +5,32 @@ app = Flask(__name__)
 
 ##########################################################################
 
+#render index
 @app.route("/")
 def index():
   return render_template("index.html")
 
+#censor_text function
+#list of censored words
+profane_terms = ["fuck"]
+
+#censor multiple
+def censor_text(usrtxt):
+  censored_text = usrtxt
+  for term in profane_terms:
+    location = censored_text.find(term)
+    if location == -1:
+      continue
+    else:
+      lot = len(term)
+      target = censored_text[location:location+lot]
+      censored_text = censored_text.replace(term, '####')
+  return censored_text
+
 @app.route("/", methods=["POST"])
 def user_input_post():
   text = request.form["text"]
-  processed_text = text.upper()
+  processed_text = censor_text(text)
   return render_template("index.html", text=text, processed_text = processed_text)
 
 
