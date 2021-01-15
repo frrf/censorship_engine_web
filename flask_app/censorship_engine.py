55ea61010541b8fe954ebfc9e8c489ec59e21204
frrf <franciscopali71@gmail.com>
2021-01-14 20:32:18 -0800

    changed the directory name of app

---
+++ flask_app/censorship_engine.py
@@ -0,0 +1,13 @@
+def censor_text(usrtxt):
+  with open("swearWords.txt") as fobj:
+    swearWordstxt = fobj.read()
+    profane_terms = swearWordstxt.split()
+  censored_text = usrtxt
+  for term in profane_terms:
+    location = censored_text.find(term)
+    if location == -1:
+      continue
+    else:
+      lot = len(term)
+      censored_text = censored_text.replace(term, '🧼🧼🧼🧼')
+  return censored_text
